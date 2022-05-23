7c0d0108d7a21ba56b0308a8ee78b5b2603a308b
kasper <kasperfriangt@gmail.com>
2022-05-23 11:12:12 +0200

    star info

---
+++ analyticsServer/AnalyticsServer/DBAccess/DBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Net;
+using System.Security;
+
+namespace Analytics_server.DBAccess
+{
+    public class DBContext
+    {
+        private NetworkCredential creds;
+        private string connectionString;
+        public DBContext()
+        {
+            creds = new NetworkCredential(Environment.GetEnvironmentVariable("DbUsername"),Environment.GetEnvironmentVariable("DbPassword"));
+            
+            connectionString = Environment.GetEnvironmentVariable("connString");
+            
+        }
+
+        
+
+    }
+}
---
+++ analyticsServer/AnalyticsServer/starInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Analytics_server
+{
+    public class starInfo
+    {
+        public starInfo(string name = null, int born = default, List<string> moviesAppearedIn = null)
+        {
+            Name = name;
+            this.born = born;
+            this.moviesAppearedIn = moviesAppearedIn;
+        }
+
+        public string Name { get; set; }
+        public int born { get; set; }
+        public List<string> moviesAppearedIn { get; set; }
+    }
+}
